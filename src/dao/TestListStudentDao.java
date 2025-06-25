5fdeb700c3f7809193b16ab1b9a6501146a5a299
aira-tachi <spr2440851@stu.o-hara.ac.jp>
2025-06-25 11:16:23 +0900

    6/25dao修正

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -6,72 +6,50 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.Subject;
-
 import bean.Student;
 import bean.Test;
+import bean.TestListStudent;
 
 public class TestListStudentDao extends Dao {
 
 	/**
-	 * postFilterメソッド
+	 * postFilterメソッド 実行結果(ResultSet)をTestListStudent型のリストに変換するメソッド
 	 *
 	 * @param resultSet:ResultSet
 	 *            実行結果
 	 * @param school:School
 	 *            所属学校
- 	 * @return テスト情報のリスト
+ 	 * @return
 	 * @throws Exception
 	 */
 	public List<TestListStudent> postFilter(ResultSet rSet) throws Exception {
-		// テスト情報リストのインスタンスを取得
+		// 学生別成績リストのインスタンスを取得
 		List<TestListStudent> list = new ArrayList<>();
 
 		// 実行結果を1件ずつ処理
-		while (resultSet.next()){
-			// 学生インスタンスを取得
+		while (rSet.next()){
+			// 学生別成績インスタンスを取得
 			Test test = new Test();
 
-			// 学生情報の一部をテストインスタンスにセット
-			Student stu = new Student();
-			stu.setNo(resultSet.getString("student_no"));
-			stu.setClassNum(resultSet.getString("class_num"));
-			stu.setSchool(school);
-			test.setStudent(stu);
+			TestListStudent tls = new TestListStudent();
+			tls.setSubjectName(rSet.getString("subject_name"));
+			tls.setSubjectCd(rSet.getString("subject_cd"));
+			tls.setNum(rSet.getInt("no"));
+			tls.setPoint(rSet.getInt("point"));
 
-
-			// 科目情報の一部をテストインスタンスにセット
-			Subject subject = new Subject();
-			subject.setCd(resultSet.getString("subject_cd"));
-			subject.setSchool(school);
-			test.setSubject(subject);
-
-			// その他の情報をテストインスタンスにセット
-			test.setSchool(school);
-			test.setNo(resultSet.getInt("no"));
-			test.setPoint(resultSet.getInt("point"));
-
-			// テスト情報リストに追加
-			list.add(test);
+			// 学生別成績リストに追加
+			list.add(tls);
 		}
 
 		return list;
 	}
 
 	/**
-	 * filterメソッド
+	 * filterメソッド 学生に紐づいたテスト情報を絞り込む
 	 *
-	 * @param entYear:int
-	 *            入学年(student)
-	 * @param classNum:String
-	 *            クラス番号(student)
-	 * @param subject:Subject
-	 *            科目(test)
-	 * @param no:int
-	 *            回数(test)
 	 * @param school:School
-	 *            所属学校(student,test)
- 	 * @return テスト情報のリスト
+	 *            学生
+ 	 * @return
 	 * @throws Exception
 	 */
 	public List<TestListStudent> filter(Student student) throws Exception {
@@ -84,25 +62,20 @@ public class TestListStudentDao extends Dao {
 
 		try {
 			// プリペアードステートメントにSQL文をセット
-			st = con.prepareStatement("SELECT * FROM test t "
-					+ "INNER JOIN student s ON t.student_no = s.no "
-					+ "WHERE s.ent_year = ? "
-					+ "AND t.class_num = ? "
-					+ "AND t.subject_cd = ? "
-					+ "AND t.no = ? "
+			st = con.prepareStatement("SELECT t.subject_cd, sub.name, t.no, t.point  FROM test t "
+					+ "INNER JOIN subject sub ON t.student_cd = sub.cd AND t.school_cd = sub.school_cd "
+					+ "WHERE t.student_no = ? "
 					+ "AND t.school_cd = ?");
 
-			// プリペアードステートメントに各条件をバインド
-			st.setInt(1, entYear);
-			st.setString(2, classNum);
-			st.setString(3, subject.getCd());
-			st.setInt(4,no) ;
-			st.setString(5, school.getCd());
+			// プリペアードステートメントに学生番号と学校コードをバインド
+			st.setString(1, student.getNo());
+			st.setString(2, student.getSchool().getCd());
+
 
 			// プリペアードステートメントを実行し、結果をセット
 			try(ResultSet rs = st.executeQuery()) {;
 				// postFilterメソッドを利用し実行結果をテスト情報リストに変換
-				list = postFilter(rs, school);
+				list = postFilter(rs);
 			}
 
 		} catch (Exception e) {
